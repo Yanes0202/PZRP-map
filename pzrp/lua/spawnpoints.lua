d63140f7f0d455ccf94f07f4bd541f9859b4acac
Adrian <60939933+Yanes0202@users.noreply.github.com>
2023-06-28 16:03:46 +0200

    all

---
+++ pzrp/lua/spawnpoints.lua
@@ -0,0 +1,28 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 47, worldY = 22, posX = 89, posY = 38, posZ = 0 },
+    { worldX = 50, worldY = 22, posX = 54, posY = 47, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 47, worldY = 22, posX = 89, posY = 38, posZ = 0 },
+    { worldX = 50, worldY = 22, posX = 54, posY = 47, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 47, worldY = 22, posX = 89, posY = 38, posZ = 0 },
+    { worldX = 50, worldY = 22, posX = 54, posY = 47, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 47, worldY = 22, posX = 89, posY = 38, posZ = 0 },
+    { worldX = 50, worldY = 22, posX = 54, posY = 47, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 47, worldY = 22, posX = 89, posY = 38, posZ = 0 },
+    { worldX = 50, worldY = 22, posX = 54, posY = 47, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 47, worldY = 22, posX = 89, posY = 38, posZ = 0 },
+    { worldX = 50, worldY = 22, posX = 54, posY = 47, posZ = 0 }
+  }
+}
+end
